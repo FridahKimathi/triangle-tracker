5f219d233939e678ab73b06021f7b7f0719ea64b
fridah kim <fredakim18@gmail.com>
2018-11-29 22:55:29 +0300

    css modified

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+function myFunction(){
+    var sides =[];
+    
+    var a =parseInt(document.getElementById("one").value);
+    var b =parseInt(document.getElementById("two").value);
+    var c =parseInt(document.getElementById("three").value);
+    
+    sides.push("a");
+    sides.push("b");
+    sides.push("c");
+    
+    var results =document.querySelector("#results");
+    
+    if(isNaN(a)){
+      results.innerHTML="Invalid entry!Please enter a number.";
+    }
+    else if(isNaN(b)){
+        results.innerHTML="Invalid entry!Please enter a number.";
+    }
+    else if(isNaN(c)){
+        results.innerHTML="Invalid entry!Please enter a number.";
+    
+    }
+    
+    else if (a==b && b==c && c==a){
+            results.innerHTML="Equilateral triangle";
+    }
+    else if((a!==b && b!==c && a!==c) && (a+b <c && b+c<a)){
+            results.innerHTML ="scalene  triangle";
+    }
+    else if(a==c || b==a || b==c){
+            results.innerHTML="isosceles triangle";
+    }
+    else if(a+b<=c || a+c<=b || b+c<=a){
+            results.innerHTML="Not a triangle";
+    
+    }
+    
+    }
+    
