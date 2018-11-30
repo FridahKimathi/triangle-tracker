1f68f46eb4212c54c56296976dd6aba45242dd88
fridah kim <fredakim18@gmail.com>
2018-11-30 15:12:09 +0300

    Readme modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function triangleTracker(){
+
     var sides =[];
     
     var a =parseInt(document.getElementById("one").value);
@@ -8,6 +8,7 @@ function triangleTracker(){
     sides.push("a");
     sides.push("b");
     sides.push("c");
+    function triangleTracker(){
     
     var results =document.querySelector("#results");
     
